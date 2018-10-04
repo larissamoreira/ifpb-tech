cdbd760bc8b2014254b5d34b7ac106c12139fff2
larissamoreira <larissamgusmao@gmail.com>
2018-10-03 21:58:22 -0300

    Correção navbar-burguer

--- app.js
+++ app.js
@@ -13,6 +13,41 @@ var html = document.querySelector('html');
     html.classList.remove('is-clipped');
 })
 */
+var myNav = document.getElementById('myNav');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 50 ) {
+        myNav.classList.add("is-success");
+        myNav.classList.remove("nav-transparent");
+    } 
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
+
 function openTab(evt, tabTitle) {
     console.log(tabTitle)
     var i, tabcontent, tabs;
